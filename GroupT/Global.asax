0fd504ead84fb080d98c50b113bd66936c514e4b
Mbali Tunzi <mtk.tunzi@gmail.com>
2021-11-04 14:01:16 +0200

    Add project files.

---
+++ GroupT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GroupT.MvcApplication" Language="C#" %>
---
+++ GroupT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GroupT.Startup))]
+namespace GroupT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
